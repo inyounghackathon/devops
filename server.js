decdb0f8ba5ae1b21c6d8504ad9edc41453141d0
Tallis Vanek <tallisvanek@dhcp-110-112.cu-wireless-staff.carleton.ca>
2016-04-01 11:48:21 -0400

    updates

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-var http = require('http');
-http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/html'
-  });
-  res.write('<!doctype html>\n<html lang="en">\n' +
-    '\n<meta charset="utf-8">\n<title>Test web page on node.js</title>\n' +
-    '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' +
-    '\n\n<h1>Euro 2012 teams</h1>\n' +
-    '<div id="content"><p>The teams in Group D for Euro 2012 are:</p><ul><li>England</li><li>France</li><li>Sweden</li><li>Ukraine</li></ul></div>' +
-    '\n\n');
-  res.end();
-}).listen(8080, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8888');
+// var http = require('http');
+// http.createServer(function(req, res) {
+//   res.writeHead(200, {
+//     'Content-Type': 'text/html'
+//   });
+//   res.write('<!doctype html>\n<html lang="en">\n' +
+//     '\n<meta charset="utf-8">\n<title>Test web page on node.js</title>\n' +
+//     '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' +
+//     '\n\n<h1>Euro 2012 teams</h1>\n' +
+//     '<div id="content"><p>The teams in Group D for Euro 2012 are:</p><ul><li>England</li><li>France</li><li>Sweden</li><li>Ukraine</li></ul></div>' +
+//     '\n\n');
+//   res.end();
+// }).listen(8080, '127.0.0.1');
+// console.log('Server running at http://127.0.0.1:8888');
 
 
 // 'use strict';
@@ -29,3 +29,10 @@ console.log('Server running at http://127.0.0.1:8888');
 //
 // app.listen(PORT);
 // console.log('Running on http://localhost:' + PORT);
+
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
